e419fb5e14c68d2c2bc56251afa59bf0ae1ffaab
razan0r <razanhamad815@gmail.com>
2024-02-10 19:16:48 +0300

    project

---
+++ src/shared/Input.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './input.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+function Input({id,name,title,type='text',onChange,errors,onBlur,touched,placeholder,icon,customClass}) {
+  {console.log(icon)}
+  return (
+   
+   <div className='form mb-3 mt-5 '>
+    <label htmlFor={id} className='form-label'>{title}</label>
+    <FontAwesomeIcon icon={icon}/>
+    <input type={type} name={name} className={`form-control ${customClass}`} id={id} onBlur={onBlur} onChange={onChange} placeholder={placeholder}/>
+    {touched[name]&&errors[name]&&<p className='text text-danger'>{errors[name]}</p>}
+   </div>
+  )
+}
+
+export default Input
